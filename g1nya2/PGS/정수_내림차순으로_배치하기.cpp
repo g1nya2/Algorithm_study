da218455b688a869685a55eb37a5e88625675903
g1nya2 <105257807+g1nya2@users.noreply.github.com>
2024-07-11 18:54:20 +0900

    [PGS] 정수 내림차순으로 배치하기

--- g1nya2/PGS/정수_내림차순으로_배치하기.cpp
+++ g1nya2/PGS/정수_내림차순으로_배치하기.cpp
@@ -9,12 +9,12 @@ long long solution(long long n) {
     long long answer = 0;
     vector <long long> tmp ;
   
-    while(n){
+    while(n){ //배열에 추가
         tmp.push_back(n%10);
         n/=10;
     }
     
-    for(long long i =0; i<tmp.size();i++){ //sort 시킴
+    for(long long i =0; i<tmp.size();i++){ //sort
         for(long long j =i; j<tmp.size();j++){
             if(tmp[i]>tmp[j]){
                 long long t = tmp[i];
@@ -24,7 +24,7 @@ long long solution(long long n) {
         }
     }
     
-    for(long long k =0; k<tmp.size();k++){
+    for(long long k =0; k<tmp.size();k++){ //자릿수에 맞춰 합체
         answer+=tmp[k]*pow(10,k);
     }
   
