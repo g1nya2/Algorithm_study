80b9de605e99679756cc0778c1372decbf39dea6
g1nya2 <105257807+g1nya2@users.noreply.github.com>
2024-07-12 07:48:02 +0900

    [PGS] 정수 제곱근 판
    https://school.programmers.co.kr/learn/courses/30/lessons/12934

---
+++ g1nya2/PGS/정수_제곱근_판별.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    if(pow(static_cast<int>(sqrt(n)),2)==n){ // sqrt정수화한 것을 제곱한것과 n이 같으면 n은 정수의 제곱
+        answer = pow(sqrt(n)+1,2);
+    }
+    else{
+        answer=-1;
+    }
+    return answer;
+}
